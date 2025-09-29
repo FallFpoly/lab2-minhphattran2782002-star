330e24225a6f0693dd1a3739bac3a77ec36c2951
ADMIN <ADMIN@172.16.91.56>
2025-09-29 21:38:01 +0700

    nộp lab2 bai2

--- Lab2/src/main/java/com/mycompany/lab2/bai1.java
+++ Lab2/src/main/java/com/mycompany/lab2/bai1.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.lab2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ICT
@@ -11,6 +13,21 @@ package com.mycompany.lab2;
 public class bai1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+                Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap he so a: ");
+        double a = scanner.nextDouble();
+        System.out.print("Nhap he so b: ");
+        double b = scanner.nextDouble();
+
+        if (a == 0) {
+            if (b == 0) {
+                System.out.println("Phuong trinh vo so nghiem");
+            } else {
+                System.out.println("Phuong trinh vo nghiem");
+            }
+        } else {
+            double x = -b / a;
+            System.out.println("Phuong trinh co nghiem x = " + x);
+        }
     }
 }
--- Lab2/src/main/java/com/mycompany/lab2/bai3.java
+++ Lab2/src/main/java/com/mycompany/lab2/bai3.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.lab2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ICT
@@ -11,6 +13,19 @@ package com.mycompany.lab2;
 public class bai3 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Nhap so dien su dung: ");
+        int soDien = scanner.nextInt();
+        int tien;
+
+        if (soDien <= 50) {
+            tien = soDien * 1000;
+        } else {
+            tien = 50 * 1000 + (soDien - 50) * 1200;
+        }
+
+        System.out.println("Tien dien phai tra: " + tien + " đong");
+        
     }
 }
